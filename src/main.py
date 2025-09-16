0c700a048961e41a3da51e3883b0feaccefe7aa1
Egor Lanschekov <egorrkaboxer@gmail.com>
2025-09-16 13:51:44 +0300

    Create blockchain project with tutorial

--- src/main.py
+++ src/main.py
@@ -1,4 +1,10 @@
 import hashlib as hasher
+import sys
+
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import (
+    QApplication, QWidget, QVBoxLayout, QPushButton, QTextEdit, QLabel
+)
 
 
 class Block:
@@ -25,26 +31,79 @@ def create_genesis_block():
     # index zero and arbitrary previous hash
     return Block(0, date.datetime.now(), "Genesis Block", "0")
 
-def next_block(last_block):
+
+def next_block(last_block, data):
   index = last_block.index + 1
   timestamp = date.datetime.now()
-  data = "Hey! I'm block " + str(index)
   return Block(index, timestamp, data, last_block.hash)
 
 
-# Create the blockchain and add the genesis block
-blockchain = [create_genesis_block()]
-last_block = blockchain[0]
+class BlockchainApp(QWidget):
+    def __init__(self):
+        super().__init__()
 
-# How many blocks should we add to the chain
-# after the genesis block
-num_of_blocks_to_add = 5
+        self.setWindowTitle("Блокчейн")
+        self.resize(800, 600)
 
-# Add blocks to the chain
-for i in range(0, num_of_blocks_to_add):
-  block_to_add = next_block(last_block)
-  blockchain.append(block_to_add)
-  last_block = block_to_add
+        # Инициализация блокчейна
+        self.blockchain = [create_genesis_block()]
+        self.last_block = self.blockchain[0]
 
-  print("Block #{} has been added to the blockchain!".format(block_to_add.index))
-  print("Hash: {}\n".format(block_to_add.hash))
+        # Элементы интерфейса
+        layout = QVBoxLayout()
+
+        self.label = QLabel("Демонстрация работы блокчейна")
+        self.label.setFont(QFont('Arial', 11))
+        layout.addWidget(self.label)
+
+        self.text_output = QTextEdit()
+        self.text_output.setFont(QFont('Arial', 11))
+        self.text_output.setReadOnly(True)
+        layout.addWidget(self.text_output)
+
+        self.add_button = QPushButton("Добавить новый блок")
+        self.add_button.setFont(QFont('Arial', 11))
+        self.add_button.clicked.connect(self.add_block)
+        layout.addWidget(self.add_button)
+
+        self.show_button = QPushButton("Показать весь блокчейн")
+        self.show_button.setFont(QFont('Arial', 11))
+        self.show_button.clicked.connect(self.show_blockchain)
+        layout.addWidget(self.show_button)
+
+        self.try_delete_button = QPushButton("Попробовать удалить блок")
+        self.try_delete_button.setFont(QFont('Arial', 11))
+        self.try_delete_button.clicked.connect(self.try_delete_block)
+        layout.addWidget(self.try_delete_button)
+
+        self.setLayout(layout)
+
+    def add_block(self):
+        new_block = next_block(self.last_block, f"Данные блока #{len(self.blockchain)}")
+        self.blockchain.append(new_block)
+        self.last_block = new_block
+
+        self.text_output.clear()
+        self.text_output.append(
+            f"\nБлок #{new_block.index} добавлен."
+        )
+
+    def show_blockchain(self):
+        self.text_output.clear()
+        self.text_output.append("\nТекущий блокчейн")
+        self.text_output.append(f"Общее количество блоков: {len(self.blockchain)}\n")
+        for block in self.blockchain:
+            self.text_output.append(f"Блок #{block.index}\n"
+                                    f"Хеш: #{block.hash}\n")
+
+    def try_delete_block(self):
+        self.text_output.clear()
+        self.text_output.append("\nОШИБКА: структура блокчейна неизменяема.")
+
+
+# Запуск приложения
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = BlockchainApp()
+    window.show()
+    sys.exit(app.exec_())
